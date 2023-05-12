03bb62a982d1f2fd84e92892af74868ddd54200b
kehindebadejo <76959584+kehindebadejo@users.noreply.github.com>
2023-05-12 17:51:15 +0100

    twitteraggregator.py

---
+++ twitteraggregator.py
@@ -0,0 +1,62 @@
+import re
+import tweepy
+from tweepy import OAuthHandler
+from textblob import TextBlob
+
+class TwitterClient(object):
+	'''
+	Generic Twitter Class for sentiment analysis.
+	'''
+	def __init__(self):
+		'''
+		Class constructor or initialization method.
+		'''
+		# keys and tokens from the Twitter Dev Console
+		consumer_key = 'cyVLURNdYf1Ss9uChPmYo41Qt'
+		consumer_secret = 'IbtNOSfLVDDsbZteRQcVt7jkCE9Dvu1bX3PcJ7p2sYGzcpdXZK'
+		access_token = '14152205-iJ8FdjMQT88jpxNAd2s8hCo6yXuJxNlLXvS11AUwf'
+		access_token_secret = 'z3i2sNB6jls9CVFad1b55pLY3eQjDSxUYB89obNC195eC'
+
+		# attempt authentication
+		try:
+			# create OAuthHandler object
+			self.auth = OAuthHandler(consumer_key, consumer_secret)
+			# set access token and secret
+			self.auth.set_access_token(access_token, access_token_secret)
+			# create tweepy API object to fetch tweets
+			self.api = tweepy.API(self.auth)
+		except:
+			print("Error: Authentication Failed")
+
+	def clean_tweet(self, tweet):
+		'''
+		Utility function to clean tweet text by removing links, special characters
+		using simple regex statements.
+		'''
+		return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])| (\w+:\/\/\S+)", " ", tweet).split())
+
+	def get_tweet_sentiment(self, tweet):
+		'''
+		Utility function to classify sentiment of passed tweet
+		using textblob's sentiment method
+		'''
+		# create TextBlob object of passed tweet text
+		analysis = TextBlob(self.clean_tweet(tweet))
+		# set sentiment
+		if analysis.sentiment.polarity > 0:
+			return 'positive'
+		elif analysis.sentiment.polarity == 0:
+			return 'neutral'
+		else:
+			return 'negative'
+
+	def get_tweets(self, query, count = 10):
+		'''
+		Main function to fetch tweets and parse them.
+		'''
+		# empty list to store parsed tweets
+		tweets = []
+
+		try:
+			# call twitter api to fetch tweets
+			fetched_tweets = self.api.search_tweets(q = query, count = count)
